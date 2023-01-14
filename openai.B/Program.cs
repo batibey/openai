6fb5feb8f4a4c5991b7a76c1c6d61008dfd7372f
mustafabati <mustafa.bati9@gmail.com>
2023-01-14 22:29:28 +0300

    openai

---
+++ openai.B/Program.cs
@@ -0,0 +1,15 @@
+﻿using openai.B.Services;
+using OpenAI.GPT3.Extensions;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddOpenAIService(settings => settings.ApiKey = "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");
+
+        services.AddHostedService<OpenAICompletionService>();
+        //services.AddHostedService<OpenAIImageService>();
+    })
+    .Build();
+
+host.Run();
+
